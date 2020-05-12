406920f2dce561d3481c9a23639f1844ac72d526
Abdelkrim Bournane <ea_bournane@esi.dz>
2020-05-12 02:54:05 +0100

    Added CoverType entity and repository

---
+++ DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repository
+{
+    class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var entityFromDb = _db.CoverTypes.Find(coverType.Id);
+            if(entityFromDb != null)
+            {
+                entityFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
