c52056044a49463759244d9ab3b4472c1f4bbc66
Abdelkrim Bournane <ea_bournane@esi.dz>
2020-05-16 05:29:28 +0100

    Added CRUD Operations for companies

---
+++ BulkyBook/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            Company company = new Company();
+
+            if (id == null)
+            {
+                return View(company);
+            }
+
+            company = _unitOfWork.Company.Get(id.GetValueOrDefault());
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return View(company);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Company.GetAll() });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.Id == 0)
+                {
+                    _unitOfWork.Company.Add(company);
+
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(company);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(company);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var company = _unitOfWork.Company.Get(id);
+            if (company == null)
+            {
+                return Json(new { success = false, message = "Problem while deleting" });
+            }
+            else
+            {
+                _unitOfWork.Company.Remove(company);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Deleted Successfully" });
+            }
+        }
+
+
+    }
+}
---
+++ BulkyBook/wwwroot/js/company.js
@@ -0,0 +1,72 @@
+﻿
+var dataTable; 
+
+$(document).ready(function () {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url":"/Admin/Company/GetAll"
+        }, 
+        "columns": [
+            {
+                "data": "name", "width":"14%"
+            }, 
+            {
+                "data": "streetAddress", "width": "14%"
+            },
+            {
+                "data": "city", "width": "14%"
+            }, 
+            {
+                "data": "state", "width": "14%"
+            },
+            {
+                "data": "postalCode", "width": "14%"
+            },
+            {
+                "data": "phoneNumber", "width": "14%"
+            },         
+            {
+                "data": "id", 
+                "render": function (data) {
+                    return `
+                          <div class="text-center">
+    <a  href ="/Admin/Company/Upsert/${data}"class="btn btn-success text-white" style="cursor:pointer;">
+        <i class="fas fa-edit"></i>
+    </a>
+    <a  onClick="Delete('/Admin/Company/Delete/${data}')" class="btn btn-danger text-white" style="cursor:pointer;">
+        <i class="fas fa-trash-alt"></i>
+    </a>
+
+</div>
+                            `
+                }, "width":"16%"
+            }
+        ]
+    });
+});
+
+
+function Delete(url) {
+    swal({
+        title: "Do you really want to delete this Cover Type",
+        text: "You will not be able to undo this action",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else
+                        toastr.error(data.message);
+                }
+            });
+        }
+    });
+}
