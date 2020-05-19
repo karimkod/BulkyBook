8b3bd683cf74f38008a0a9acf6092b16b6761656
Abdelkrim Bournane <ea_bournane@esi.dz>
2020-05-19 06:04:17 +0100

    Added user managemenet for Admin

---
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Repository;
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using BulkyBook.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var users = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (ApplicationUser user in users)
+            {
+                var userRole = userRoles.FirstOrDefault(r => r.UserId == user.Id);
+                user.Role = roles.FirstOrDefault(r => r.Id == userRole.RoleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    
+                    };
+                }
+
+            }
+
+            return Json(new { data = users });
+        }
+
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var userFromDb = _db.ApplicationUsers.Find(id); 
+            if(userFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }else
+            {
+                if(userFromDb.LockoutEnd != null && userFromDb.LockoutEnd > DateTime.Now)
+                {
+                    userFromDb.LockoutEnd = DateTime.Now; 
+                }else
+                {
+                    userFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+                }
+                _db.SaveChanges();
+                return Json(new { success = true, message = "Operation successful" });
+
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ BulkyBook/wwwroot/js/user.js
@@ -0,0 +1,79 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            {
+                "data": "name", "width": "12%"
+            },
+            {
+                "data": "email", "width": "12%"
+            },
+            {
+                "data": "streetAddress", "width": "12%"
+            },
+            {
+                "data": "phoneNumber", "width": "12%"
+            },
+            {
+                "data": "role", "width": "12%"
+            },
+            {
+                "data": "company.name", "width": "12%"
+            },
+            {
+                "data": { id: "id", lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        return `
+                        <div class="text-center">
+
+    <a  onclick="LockUnlock('${data.id}')" class="btn btn-success text-white" style="cursor:pointer;">
+        <i class="fas fa-lock-open"></i> Unlock
+    </a>
+
+</div>
+                            `;
+                    } else {
+                        return `
+                        <div class="text-center">
+
+    <a  onclick="LockUnlock('${data.id}')" class="btn btn-danger text-white" style="cursor:pointer;">
+        <i class="fas fa-lock"></i> Lock
+    </a>
+
+</div>
+                            `;
+                    }
+                }
+                    
+                , "width": "28%"
+            }
+        ]
+    });
+});
+
+function LockUnlock(id) {
+
+    $.ajax({
+        type: "POST",
+        url: "/Admin/User/LockUnlock",
+        data: JSON.stringify(id),
+        contentType : "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+            else
+                toastr.error(data.message);
+        }
+    });
+
+}
