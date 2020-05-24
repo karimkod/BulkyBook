514f76e4dabf1be014c98b179905750f493283c6
Abdelkrim Bournane <ea_bournane@esi.dz>
2020-05-24 06:04:08 +0100

    Added Utility methods to return price based on quantity and a method to convert string to raw html

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -19,6 +19,44 @@ namespace Utility
         public const string Role_Employee = "Employee";
 
 
+        public static double GetPriceDependingOnQuantity(int quantity, double price, double price50, double price100)
+        {
+            if (quantity < 49)
+                return price; 
+            else if (quantity < 99)
+                return price50;
+            else
+                return price100;
+        }
+
+        public static string ConvertToRawHtml(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
+
 
     }
 } 
