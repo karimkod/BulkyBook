dcb9fc03401765369b10ef7a356de0c5be07bea3
Abdelkrim Bournane <ea_bournane@esi.dz>
2020-05-25 13:18:29 +0100

    Added cart view

---
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.ViewModels;
+using Utility;
+
+namespace BulkyBook.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager; 
+
+        public CartController(IUnitOfWork unitOfWork,
+            IEmailSender emailSender,
+            UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+
+        public ShoppingCartVM shoppingCartVM { get; set; }
+
+        public IActionResult Index()
+        {
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            shoppingCartVM = new ShoppingCartVM()
+            {
+                orderHeader = new OrderHeader(),
+                shoppingCarts = _unitOfWork.ShoppingCart.GetAll(s => s.ApplicationUserId == claim.Value, includeProperties:"Product")
+            };
+
+            shoppingCartVM.orderHeader.OrderTotal = 0;
+            shoppingCartVM.orderHeader.ApplicationUserId = claim.Value;
+            shoppingCartVM.orderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value, includeProperties: "Company");
+            
+
+            foreach(ShoppingCart cart in shoppingCartVM.shoppingCarts)
+            {
+                cart.Price = SD.GetPriceDependingOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                shoppingCartVM.orderHeader.OrderTotal += (cart.Price * cart.Count);
+                cart.Product.Description = SD.ConvertToRawHtml(cart.Product.Description??""); 
+                if(cart.Product.Description.Length > 100)
+                {
+                    cart.Product.Description = cart.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            
+
+            return View(shoppingCartVM);
+        }
+    }
+}
---
+++ Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public OrderHeader orderHeader { get; set; }
+        public IEnumerable<ShoppingCart> shoppingCarts { get; set; }
+
+    }
+}
